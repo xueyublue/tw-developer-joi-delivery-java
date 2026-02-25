b03c8b381de4affdb11408e9c4e4d3fddbe294ee
Darren <xueyublue@gmail.com>
2026-02-25 23:56:01 +0800

    Validate request body

--- src/main/java/com/tw/joi/delivery/controller/InventoryController.java
+++ src/main/java/com/tw/joi/delivery/controller/InventoryController.java
@@ -2,6 +2,8 @@ package com.tw.joi.delivery.controller;
 
 import com.tw.joi.delivery.domain.GroceryProduct;
 import com.tw.joi.delivery.service.InventoryService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +21,7 @@ public class InventoryController {
     private final InventoryService inventoryService;
 
     @GetMapping("/health")
-    public ResponseEntity<Set<GroceryProduct>> fetchStoreInventoryHealth(@RequestParam(name = "storeId") String storeId) {
+    public ResponseEntity<Set<GroceryProduct>> fetchStoreInventoryHealth(@Valid @NotBlank @RequestParam(name = "storeId") String storeId) {
         return ResponseEntity.ok(inventoryService.getInventoryForStore(storeId));
     }
 }
